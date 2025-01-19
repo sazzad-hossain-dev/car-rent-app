a4a1d91ab10d90580e8d8e5500f4fe084e8aff20
phenomop <bsgametube785@gmail.com>
2025-01-19 08:48:45 +0600

    car key changed

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,17 @@ import SearchBar from "@/components/SearchBar";
 import { fuels, yearsOfProduction } from "@/constants";
 import { fetchCars } from "@/utils";
 
-export default async function Home({ searchParams }) {
+export default async function Home({
+    searchParams,
+}: {
+    searchParams: {
+        manufacturer?: string;
+        year?: number;
+        fuel?: string;
+        limit?: number;
+        model?: string;
+    };
+}) {
     const allCars = await fetchCars({
         manufacturer: searchParams.manufacturer || "",
         year: searchParams.year || 2022,
@@ -13,14 +23,15 @@ export default async function Home({ searchParams }) {
         limit: searchParams.limit || 10,
         model: searchParams.model || "",
     });
+
     const isDataEmpty =
         !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
     return (
         <main className="overflow-hidden">
             <Hero />
-            <div className="mt-12 padding-x padding-y max-width " id="discover">
-                <div className="home__text-container ">
+            <div className="mt-12 padding-x padding-y max-width" id="discover">
+                <div className="home__text-container">
                     <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
                     <p>Explore the cars you might like</p>
                 </div>
@@ -37,15 +48,15 @@ export default async function Home({ searchParams }) {
                 {!isDataEmpty ? (
                     <section>
                         <div className="home__cars-wrapper">
-                            {allCars.map((car) => (
-                                <CarCard key={car.year} car={car} />
+                            {allCars.map((car, index) => (
+                                <CarCard key={index} car={car} />
                             ))}
                         </div>
                     </section>
                 ) : (
                     <div className="home__error-container">
                         <h3 className="text-black text-xl font-bold">
-                            Opps, No results{" "}
+                            Opps, No results
                         </h3>
                     </div>
                 )}
